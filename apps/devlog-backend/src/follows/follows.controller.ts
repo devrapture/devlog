92ae43ecbc2788fb2751e0dceb92d5d75116596a
devrapture <coolpythoncodes@gmail.com>
2025-09-17 13:45:41 +0100

    feat(follows): implement follow and unfollow logic
    This commit fully implements the follow and unfollow functionality in the `FollowsService`.
    
    - Implements the `followUser` and `unfollowUser` methods to create and delete follow relationships.
    - Updates user follower and following counts transactionally.
    - Adds error handling for various scenarios like self-following or non-existent users.
    - Adds new endpoints to the `FollowsController` to get a user's followers and following lists.

--- apps/devlog-backend/src/follows/follows.controller.ts
+++ apps/devlog-backend/src/follows/follows.controller.ts
@@ -1,25 +1,55 @@
-import { Controller, Delete, Param, ParseUUIDPipe, Post, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { GetUser } from 'src/auth/decorator/user.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { FollowsService } from './follows.service';
 import { ApiAuthenticatedEndpoint } from 'src/common/decorators/api-responses.decorator';
+import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
 import { SuccessResponseDto } from 'src/common/dto/responses/success-response.dto';
+import { FollowsService } from './follows.service';
 
 @Controller('follows')
 export class FollowsController {
-  constructor(private readonly followsService:FollowsService){}
+  constructor(private readonly followsService: FollowsService) {}
 
-  @Post(":id")
+  @Post(':id')
   @UseGuards(JwtAuthGuard)
-  @ApiAuthenticatedEndpoint('Follow User',200,SuccessResponseDto)
-  followUser(@GetUser('id') userId, @Param('id', ParseUUIDPipe) followingId:string){
-    return this.followsService.followUser(userId,followingId)
+  @ResponseMessage('User is following successfully')
+  @ApiAuthenticatedEndpoint('Follow User', 200, SuccessResponseDto)
+  followUser(
+    @GetUser('id') userId: string,
+    @Param('id', ParseUUIDPipe) followingId: string,
+  ) {
+    return this.followsService.followUser(userId, followingId);
   }
 
   @Delete(':id')
   @UseGuards(JwtAuthGuard)
-  @ApiAuthenticatedEndpoint('Unfollow user',200,SuccessResponseDto)
-  unfollowUser(@GetUser('id') userId, @Param('id', ParseUUIDPipe) followingId:string){
-return this.followsService.unfollowUser(userId,followingId)
+  @ApiAuthenticatedEndpoint('Unfollow user', 200, SuccessResponseDto)
+  unfollowUser(
+    @GetUser('id') userId: string,
+    @Param('id', ParseUUIDPipe) followingId: string,
+  ) {
+    return this.followsService.unfollowUser(userId, followingId);
+  }
+
+  @Get('following')
+  @UseGuards(JwtAuthGuard)
+  @ApiAuthenticatedEndpoint('Get following', 200, SuccessResponseDto)
+  getFollowing(@GetUser('id') userId: string) {
+    return this.followsService.getFollowing(userId);
+  }
+
+  @Get('followers')
+  @UseGuards(JwtAuthGuard)
+  @ApiAuthenticatedEndpoint('Get followers', 200, SuccessResponseDto)
+  getFollowers(@GetUser('id') userId: string) {
+    return this.followsService.getFollowers(userId);
   }
 }
--- apps/devlog-backend/src/follows/follows.service.ts
+++ apps/devlog-backend/src/follows/follows.service.ts
@@ -1,20 +1,118 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { Follow } from './entities/follow.entity';
 
 @Injectable()
 export class FollowsService {
-  constructor(){}
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @InjectRepository(Follow)
+    private readonly followRepository: Repository<Follow>,
+  ) {}
 
-  followUser(userId,followingId){
-    return {
-      userId,
-      followingId
+  async followUser(userId: string, followingId: string) {
+    if (userId === followingId) {
+      throw new Error('You cannot follow yourself');
     }
+
+    const following = await this.userRepository.findOne({
+      where: {
+        id: followingId,
+      },
+    });
+
+    if (!following) throw new NotFoundException('User to follow not found');
+
+    const user = await this.userRepository.findOne({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) throw new NotFoundException('User not found');
+
+    const existingFollow = await this.followRepository.findOne({
+      where: {
+        following: {
+          id: followingId,
+        },
+        follower: {
+          id: userId,
+        },
+      },
+    });
+
+    if (existingFollow)
+      throw new ConflictException('User is already following');
+
+    await this.followRepository.manager.transaction(async (manager) => {
+      // Create follow record
+      await manager.save(Follow, {
+        follower: { id: userId },
+        following: { id: followingId },
+      });
+
+      // Increment follow count
+      await manager.increment(User, { id: userId }, 'followingCount', 1);
+      await manager.increment(User, { id: followingId }, 'followersCount', 1);
+    });
+
+    return;
   }
 
-  unfollowUser(userId,followingId){
-    return {
-      userId,
-      followingId
-    }
+  async unfollowUser(userId: string, followingId: string) {
+    const following = await this.userRepository.findOne({
+      where: {
+        id: followingId,
+      },
+    });
+
+    if (!following) throw new NotFoundException('User to unfollow not found');
+
+    const user = await this.userRepository.findOne({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) throw new NotFoundException('User not found');
+
+    const existingFollow = await this.followRepository.findOne({
+      where: {
+        following: {
+          id: followingId,
+        },
+        follower: {
+          id: userId,
+        },
+      },
+    });
+
+    if (!existingFollow)
+      throw new NotFoundException('User is not following this user');
+
+    await this.followRepository.manager.transaction(async (manager) => {
+      // Remove follow record
+      await manager.remove(Follow, existingFollow);
+
+      // Decrement follow count
+      await manager.decrement(User, { id: userId }, 'followingCount', 1);
+      await manager.decrement(User, { id: followingId }, 'followersCount', 1);
+    });
+    return;
+  }
+
+  getFollowing(userId: string) {
+    return userId;
+  }
+
+  getFollowers(userId: string) {
+    return userId;
   }
 }
